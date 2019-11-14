ad68aef660354503855aa277eb8e538e3ba4b66a
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-14 20:09:21 +0700

    controller user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,13 +1,30 @@
 import User, { IUser } from '../models/user'
+import { check } from '../helpers/bcryptjs'
+import { createToken } from '../helpers/jwt'
 
 class UserController {
-  static async create(req: any, res: any, next: Function) {
+  static async register(req: any, res: any, next: Function) {
     try {
       const { email, password, tags } = req.body
-      const newUser: IUser = await User.create({ email, password, tags })
-      res.status(201).json({ _id: newUser._id, email: newUser.email })
+      const user: IUser = await User.create({ email, password, tags })
+      res.status(201).json({ _id: user._id, email: user.email })
     } catch (e) {
       next(e)
+    }
+  }
+
+  static async login(req: any, res: any, next: Function) {
+    try {
+      const { email, password } = req.body
+      const user: IUser = await User.findOne({ email })
+      if (!user && !check(password, user.password)) {
+        next({ status: 400, message: 'Wrong Email / Password' })
+      } else {
+        const token = createToken({ _id: user._id })
+        res.status(200).json({ token })
+      }
+    } catch (error) {
+      next(error)
     }
   }
 }
