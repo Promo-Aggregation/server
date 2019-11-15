2b1fb0687f252dd080ac01f2c4877a2c5ff544b8
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-15 11:45:23 +0700

    finish testing user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import users from './users'
 
 const router = Router()
 
 router.get('/', (req, res) =>
   res.status(200).json({ message: 'Server test ok!' })
 )
+
+router.use('/users', users)
 
 export default router
