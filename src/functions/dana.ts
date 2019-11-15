27d776e166b6d288ee00d9c952c1e6e4cb91bcbd
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-15 21:16:39 +0700

    puppeteer playground

--- src/functions/dana.ts
+++ src/functions/dana.ts
@@ -1,43 +1,49 @@
 import pptr from 'puppeteer'
 
 export async function danaFood() {
-  const browser = await pptr.launch({
-    headless: true,
-    defaultViewport: { width: 1100, height: 600 }
-  })
-  const data = []
-  try {
-    const page = await browser.newPage()
-    page.setDefaultTimeout(0)
-    await page.goto('https://dana.id/promo/cate/Food/1', { waitUntil: 'networkidle2', timeout: 0 })
-    await page.waitForSelector('.section')
+  return new Promise(async (resolve, reject) => {
+    const browser = await pptr.launch({
+      headless: true,
+      defaultViewport: { width: 1100, height: 600 }
+    })
+    const data = []
+    try {
+      const page = await browser.newPage()
+      page.setDefaultTimeout(0)
+      await page.goto('https://dana.id/promo/cate/Food/1', {
+        waitUntil: 'networkidle2',
+        timeout: 0
+      })
+      await page.waitForSelector('.section')
 
-    const cards = await page.$$('.card_promo')
+      const cards = await page.$$('.card_promo')
 
-    for (const card of cards) {
-      console.log('wait for it')
-      const _title = card.$eval('h3', (h3: any) => h3.innerText)
-      const _date = card.$eval('div[class="date"]', (date: any) => date.innerText)
-      const _detailUrl = card.$eval('a[class="btn_more"]', (date: any) => date.getAttribute('href'))
-      const _imageUrl = card.$eval('.img_con > img', (node: any) => node.getAttribute('src'))
-      const [title, date, detailUrl, imageUrl] = await Promise.all([
-        _title,
-        _date,
-        _detailUrl,
-        _imageUrl
-      ])
-      data.push({ title, date, detailUrl, imageUrl, kodePromo: '' })
+      for (const card of cards) {
+        console.log('wait for it')
+        const _title = card.$eval('h3', (h3: any) => h3.innerText)
+        const _date = card.$eval('div[class="date"]', (date: any) => date.innerText)
+        const _detailUrl = card.$eval('a[class="btn_more"]', (date: any) =>
+          date.getAttribute('href')
+        )
+        const _imageUrl = card.$eval('.img_con > img', (node: any) => node.getAttribute('src'))
+        const [title, date, detailUrl, imageUrl] = await Promise.all([
+          _title,
+          _date,
+          _detailUrl,
+          _imageUrl
+        ])
+        data.push({ title, date, detailUrl, imageUrl, kodePromo: '' })
+      }
+
+      console.log(data)
+      console.log('ITS SHOWING')
+      browser.close().then(() => resolve(data))
+      // return data
+    } catch (err) {
+      console.log(err)
+      await browser.close()
     }
-
-    console.log(data)
-    console.log('ITS SHOWING')
-    await browser.close()
-
-    return data
-  } catch (err) {
-    console.log(err)
-    await browser.close()
-  }
+  })
 }
 
 export async function danaEntermainment() {
