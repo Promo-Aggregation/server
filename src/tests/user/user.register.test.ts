b3aa8be105fe3bbe058ff1cfd181dc3aa9e2d146
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-15 09:53:31 +0700

    update testing user

--- src/tests/user/user.register.test.ts
+++ src/tests/user/user.register.test.ts
@@ -2,19 +2,29 @@ import 'mocha'
 
 import chai from 'chai'
 import chaiHttp from 'chai-http'
-import { User, IUser } from '../../models'
+import { User } from '../../models'
 import app from '../../app'
 
 const expect = chai.expect
+let olduser: any
 
 chai.use(chaiHttp)
 
 describe('User Register Testing', () => {
   before(async () => {
-    const user = { email: 'tigor@email.com', password: '12345' }
-    const res = await chai
+    const user = { email: 'dummy@email.com', password: '12345' }
+    olduser = await chai
       .request(app)
       .post('/users/register')
       .send(user)
+  })
+
+  after(async () => {
+    await User.deleteMany({})
+  })
+
+  describe('Success User Register', () => {
+    console.log('masuk')
+    console.log(olduser)
   })
 })
