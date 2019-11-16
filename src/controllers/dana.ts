8598c6d509ca0603d594ad1074a910310a660614
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-16 17:30:05 +0700

    up

--- src/controllers/dana.ts
+++ src/controllers/dana.ts
@@ -1,12 +1,10 @@
-import { Promo, IPromo, IPromoModel } from '../models/promo'
 import { danaFood, danaGame, danaEntertainment } from '../functions/dana'
 
 class DanaFetchController {
   static async danaFood(req: any, res: any, next: Function) {
     try {
       const foods: any[] = await danaFood()
-      const data: IPromoModel[] = await Promo.insertMany(foods)
-      res.status(200).json(data)
+      res.status(200).json(foods)
     } catch (err) {
       next(err)
     }
@@ -15,8 +13,7 @@ class DanaFetchController {
   static async danaGame(req: any, res: any, next: Function) {
     try {
       const games: any[] = await danaGame()
-      const data: IPromoModel[] = await Promo.insertMany(games)
-      res.status(200).json(data)
+      res.status(200).json(games)
     } catch (err) {
       next(err)
     }
@@ -25,8 +22,7 @@ class DanaFetchController {
   static async danaEntertainment(req: any, res: any, next: Function) {
     try {
       const entertainments: any[] = await danaEntertainment()
-      const data: IPromoModel[] = await Promo.insertMany(entertainments)
-      res.status(200).json(data)
+      res.status(200).json(entertainments)
     } catch (err) {
       next(err)
     }
