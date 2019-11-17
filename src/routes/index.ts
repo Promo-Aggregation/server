c318bd6d4dad62a739187e4404117098b218fe2b
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-17 14:52:45 +0700

    plugged subscription controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import users from './users'
 import fetch from './fetch'
 import promos from './promos'
+import subscribe from './subscribe'
 
 const router = Router()
 
@@ -10,6 +11,7 @@ router.get('/', (req, res) => res.status(200).json({ message: 'Server test ok!' 
 router.use('/users', users)
 router.use('/fetch', fetch)
 router.use('/promos', promos)
+router.use('/subscribe', subscribe)
 
 router.get('/*', (req, res, next) =>
   next({
