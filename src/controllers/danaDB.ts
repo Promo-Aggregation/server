6b9fe00020476adb5e2286dc847e6e2991b349a9
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-18 11:29:32 +0700

    added new fetch no duplicate endpoint

---
+++ src/controllers/danaDB.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express'
+import { danaFood, danaGame, danaEntertainment } from '../functions/dana'
+import { Promo, IPromoModel } from '../models'
+
+class DanaFetchDB {
+  static async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const [foods, games, entertainments] = await Promise.all([
+        danaFood(),
+        danaGame(),
+        danaEntertainment()
+      ])
+      const fetched = [...foods, ...games, ...entertainments]
+      const dbData: IPromoModel[] = await Promo.find({})
+      const filtered: IPromoModel[] = fetched.filter(el => !dbData.some(dbDatum => el.title === dbDatum.title))
+      if (filtered.length) {
+        const newPromos: IPromoModel[] = await Promo.insertMany(filtered)
+        res.status(200).json(newPromos)
+      } else {
+        res.status(200).json(filtered)
+      }
+    } catch (e) {
+      next(e)
+    }
+  }
+}
+
+export default DanaFetchDB
