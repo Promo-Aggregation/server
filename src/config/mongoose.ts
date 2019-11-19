ef36f078693e95c4423139da3144f74ffa4448e5
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-19 14:48:00 +0700

    up

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -3,10 +3,10 @@ import { connect } from 'mongoose'
 const n = process.env.NODE_ENV
 
 const environment = n || ''
-const database = process.env.DATABASE || 'Promo-Aggregator'
+const database = process.env.DATABASE || 'mole'
 const uri = n
   ? `mongodb://localhost:27017/${database}-${environment}`
-  : 'mongodb+srv://admin:admin@tigorhutasuhut-vdnzf.mongodb.net/Process-Aggregator-test?retryWrites=true&w=majority'
+  : 'mongodb+srv://admin:admin@tigorhutasuhut-vdnzf.mongodb.net/mole?retryWrites=true&w=majority'
 ;(async () => {
   try {
     await connect(uri, {
--- src/controllers/push.ts
+++ src/controllers/push.ts
@@ -1,33 +1,12 @@
 import { Request, Response, NextFunction } from 'express'
 import { User, IUserModel } from '../models'
-import axios, { AxiosPromise } from 'axios'
+import axios from 'axios'
 
 export default class PushController {
   static async push(req: Request, res: Response, next: NextFunction) {
     try {
       const users: IUserModel[] = await User.find({}).select('device_token')
-
-      // let arr: AxiosPromise[] = users.map((user: IUserModel) => {
-      //   if (!user.device_token) return null
-      //   return axios({
-      //     url: 'https://exp.host/--/api/v2/push/send',
-      //     method: 'POST',
-      //     data: {
-      //       to: user.device_token,
-      //       body: 'You have 1 new promo'
-      //     },
-      //     headers: {
-      //       host: 'exp.host',
-      //       accept: 'application/json',
-      //       'content-type': 'application/json'
-      //     }
-      //   })
-      // })
-
-      // console.log(arr)
-      // await Promise.all(arr)
       let arr = []
-      console.log(users)
       users.forEach((user: IUserModel) => {
         if (!user.device_token) return
         arr.push(user.device_token)
